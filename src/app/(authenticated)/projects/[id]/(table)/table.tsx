2dbe4a42ee2087e81931cda6fcc991b967b08df4
ohpxho <lemuel.k.costuna.so@gmail.com>
2025-08-09 00:26:41 +0800

    added: content editable div for project desc

--- src/app/(authenticated)/projects/[id]/(table)/table.tsx
+++ src/app/(authenticated)/projects/[id]/(table)/table.tsx
@@ -75,14 +75,6 @@ export function ModulesTable({ modules }: PropTypes) {
 	return (
 		<div className="w-full">
 			<div className="flex items-center py-4">
-				{/* <Input
-					placeholder="Filter emails..."
-					value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
-					onChange={(event) =>
-						table.getColumn("email")?.setFilterValue(event.target.value)
-					}
-					className="max-w-sm"
-				/> */}
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<Button variant="outline" className="ml-auto">
--- src/app/(authenticated)/projects/[id]/desc.tsx
+++ src/app/(authenticated)/projects/[id]/desc.tsx
@@ -1,15 +1,58 @@
 "use client";
 
 import { Project } from "@/lib/types";
+import { useState, useEffect, useRef } from "react";
+import { marked } from "marked";
 
 interface PropType {
 	data: Project;
 }
 
 export default function ProjectDesc({ data }: PropType) {
+	const editorRef = useRef<HTMLDivElement>(null);
+  const content = useRef<string>(data?.description || undefined);
+  
+  const onContentChange = () => {
+    content.current = editorRef.current?.innerText;
+    console.log(content.current)
+  }
+
+ const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+  if (e.key === "Enter" && e.shiftKey) {
+    e.preventDefault();
+
+    const selection = window.getSelection();
+    if (!selection?.rangeCount) return;
+
+    const range = selection.getRangeAt(0);
+    range.deleteContents();
+
+    // Insert a <br> (line break)
+    const br = document.createElement("br");
+    range.insertNode(br);
+
+    // Move cursor after the <br>
+    range.setStartAfter(br);
+    range.setEndAfter(br);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+};
+
 	return (
-		<div className="">
-			<div>{`${data?.createdAt}`}</div>
+		<div className="w-full">
+			<div className="mb-4 text-sm text-gray-500">
+				Created: {new Date(data?.createdAt).toLocaleDateString()}
+			</div>
+      <div 
+        ref={editorRef}
+        contentEditable
+        className="focus:border-none outline-none border-none"
+        onInput={onContentChange}
+        onKeyDown={onKeyDown}
+      >
+        { content? content.current: "" }
+      </div>		
 		</div>
 	);
 }
